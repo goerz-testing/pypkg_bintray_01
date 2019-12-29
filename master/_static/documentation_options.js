2ecb43f9d57041b5da70404addd0fb159b98b8d6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-12-29 05:25:24 +0000

    Update docs after building Travis build 9 of goerz-testing/pypkg_bintray_01
    The docs were built from the branch 'master' against the commit
    f7fe93cb0299ecae6a0b492fcb612edc28b0ec21.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/goerz-testing/pypkg_bintray_01/jobs/630522712.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.0/lib/python3.8/site-packages/doctr/__main__.py deploy --key-path docs/doctr_deploy_key.enc '--command=git show f7fe93cb0299ecae6a0b492fcb612edc28b0ec21:.travis/doctr_post_process.py > post_process.py && git show f7fe93cb0299ecae6a0b492fcb612edc28b0ec21:.travis/versions.py > versions.py && python post_process.py' --built-docs docs/_build/html --no-require-master --build-tags master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1-dev (4c1f51d)',
+    VERSION: '0.0.1-dev (f7fe93c)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
